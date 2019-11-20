f5f868bd7ae5674750c03045df5f6812ffdad3ce
kimvu <kim_13.05.1995@hotmail.com>
2019-11-20 11:54:45 +0100

    ad

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,11 @@ import Fib from './Fib'
 function App() {
   return (
     <Router>
+      <header className="App-header">
+        <h1 className="App-title">
+          Fib Calc
+        </h1>
+      </header>
       <div className="App">
         <Link to="/">HOME</Link>
         <Link to="/otherpage">other page</Link>        
