25c0606f09ffef597e2ada649ea6544ebed515f7
kimvu <kim_13.05.1995@hotmail.com>
2019-11-27 10:48:40 +0100

    plz work

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import NewOtherPage from './NewOtherPage'
+import NewOtherPage from './NewOtherPage';
 import Fib from './Fib';
 
 class App extends Component {
